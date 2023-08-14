2445ea15695f13ccf1668d53a63f428ef0de9ad5
Sven Erik Knop <seknop@gmail.com>
2023-08-14 15:19:53 +0100

    Context Wrapper with cleanup

--- src/main/java/io/confluent/security/auth/provider/ldap/AuthenticationVerifier.java
+++ src/main/java/io/confluent/security/auth/provider/ldap/AuthenticationVerifier.java
@@ -12,6 +12,25 @@ import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapContext;
 import java.util.Hashtable;
 import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
+class ContextAutoClosingWrapper<T extends InitialDirContext> implements AutoCloseable, Supplier<InitialDirContext> {
+    private final T context;
+
+    public ContextAutoClosingWrapper(T s) {
+        context = s;
+    }
+
+    @Override
+    public T get() {
+        return context;
+    }
+
+    @Override
+    public void close() throws NamingException {
+        context.close();
+    }
+}
 
 @CommandLine.Command(name = "AuthenticationVerifier")
 public class AuthenticationVerifier extends LdapVerifierBase implements Callable<Integer> {
@@ -25,30 +44,31 @@ public class AuthenticationVerifier extends LdapVerifierBase implements Callable
 
     private String getUserDn(@NotNull LdapConfig config) throws NamingException {
         var env = config.ldapContextEnvironment;
-        LdapContext context = new InitialLdapContext(env, null);
 
-        SearchControls searchControls = new SearchControls();
-        searchControls.setReturningAttributes(new String[]{config.userNameAttribute});
-        searchControls.setSearchScope(config.userSearchScope); // TODO think about userPasswordAttribute
+        try (var wrapper = new ContextAutoClosingWrapper<>(new InitialLdapContext(env, null))) {
+            LdapContext context = wrapper.get();
 
-        Object[] filterArgs = new Object[] {userName};
-        NamingEnumeration<SearchResult> enumeration = context.search(config.userSearchBase, config.userDnSearchFilter, filterArgs,searchControls);
+            SearchControls searchControls = new SearchControls();
+            searchControls.setReturningAttributes(new String[]{config.userNameAttribute});
+            searchControls.setSearchScope(config.userSearchScope); // TODO think about userPasswordAttribute
 
-        if (!enumeration.hasMore()) {
-            System.err.printf("User '%s' not found %n%n", userName);
-            System.exit(-1);
+            Object[] filterArgs = new Object[]{userName};
+            NamingEnumeration<SearchResult> enumeration = context.search(config.userSearchBase, config.userDnSearchFilter, filterArgs, searchControls);
+
+            if (!enumeration.hasMore()) {
+                System.err.printf("User '%s' not found %n%n", userName);
+                System.exit(-1);
+            }
+
+            SearchResult result = enumeration.next();
+
+            if (enumeration.hasMore()) {
+                System.err.printf("Search found multiple entries for user '%s'%n", userName);
+                System.exit(-2);
+            }
+
+            return result.getNameInNamespace();
         }
-
-        SearchResult result = enumeration.next();
-
-        if (enumeration.hasMore()) {
-            System.err.printf("Search found multiple entries for user '%s'%n", userName);
-            System.exit(-2);
-        }
-
-        context.close();
-
-        return result.getNameInNamespace();
     }
 
     private void verifyPassword(String userDn, @NotNull LdapConfig config) {
@@ -57,13 +77,13 @@ public class AuthenticationVerifier extends LdapVerifierBase implements Callable
         env.put(LdapContext.SECURITY_PRINCIPAL, userDn);
         env.put(LdapContext.SECURITY_CREDENTIALS, password);
 
-        try (var ignored = autoCloseable(new InitialDirContext(env))) {
-            System.out.printf("User %s has been authenticated%n", userName);
+        try (var ignored = new ContextAutoClosingWrapper<>(new InitialDirContext(env))) {
+            System.out.printf("User '%s' has been authenticated%n", userName);
         } catch (Exception e) {
             if (e instanceof CommunicationException) {
-                System.err.printf("LDAP bind failed for user DN %s%n", userDn);
+                System.err.printf("LDAP bind failed for user DN '%s'%n", userDn);
             } else {
-                System.err.printf("LDAP bind failed for user DN %s with specified password%n", userDn);
+                System.err.printf("LDAP bind failed for user DN '%s' with specified password%n", userDn);
             }
 
             System.exit(3);
